b0455c5bceabc40b671667103e16ef1bca4e1229
Max Claus Nunes <maxcnunes@gmail.com>
2017-07-18 01:28:45 -0300

    Add functional test for not registered route

---
+++ functional_tests/not_registered_route_test.go
@@ -0,0 +1,43 @@
+// nolint dupl
+package functional_tests
+
+import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/maxcnunes/httpfake"
+)
+
+// TestNotRegisteredRoute tests a fake server handling a GET request
+// for a not registered route
+func TestNotRegisteredRoute(t *testing.T) {
+	fakeService := httpfake.New()
+	defer fakeService.Server.Close()
+
+	// register a handler for our fake service
+	fakeService.NewHandler().
+		Get("/users").
+		Reply(200).
+		BodyString(`[{"username": "dreamer"}]`)
+
+	res, err := http.Get(fakeService.ResolveURL("/clients"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close() // nolint errcheck
+
+	// Check the status code is what we expect
+	if status := res.StatusCode; status != 404 {
+		t.Errorf("request returned wrong status code: got %v want %v",
+			status, 404)
+	}
+
+	// Check the response body is what we expect
+	expected := ""
+	body, _ := ioutil.ReadAll(res.Body)
+	if bodyString := string(body); bodyString != expected {
+		t.Errorf("request returned unexpected body: got %v want %v",
+			bodyString, expected)
+	}
+}
