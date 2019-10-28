f99cc39052624c88fbcdf6d2d5a89d4416eff6fe
Max Claus Nunes <maxcnunes@gmail.com>
2019-10-28 10:35:55 -0300

    Add support to register body from struct data

--- response.go
+++ response.go
@@ -1,6 +1,10 @@
 package httpfake
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 // Response stores the settings defined by the request handler
 // of how it will respond the request back
@@ -38,5 +42,16 @@ func (r *Response) AddHeader(key, value string) *Response {
 // BodyString sets the response body
 func (r *Response) BodyString(body string) *Response {
 	r.BodyBuffer = []byte(body)
+	return r
+}
+
+// BodyStruct sets the response body from a struct
+func (r *Response) BodyStruct(body interface{}) *Response {
+	b, err := json.Marshal(body)
+	if err != nil {
+		printError(fmt.Sprintf("marshalling body %#v failed with %v", body, err))
+	}
+
+	r.BodyBuffer = b
 	return r
 }
